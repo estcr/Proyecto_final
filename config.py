ff235d33ea9a7b3143c112706f7690b2233b1e09
estcr <est_cr@hotmail.com>
2025-01-09 13:06:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import streamlit as st
 
 
 
-# Configuración de la conexión a la base de datos
-db_user = "root"  # Usuario predeterminado
-db_password = st.secrets["database"]["db_pass"],
-db_host = "34.175.207.112"  # Host predeterminado (local)
-db_name = "travel_planner"  # Nombre de la base de datos
-
 # Función para conectarse a la base de datos
 def conectar_bd():
+    # Configuración de la conexión a la base de datos
+    db_user = "root"  # Usuario predeterminado
+    db_password = st.secrets["database"]["db_pass"]
+    db_host = "34.175.207.112"  # Host predeterminado (local)
+    db_name = "travel_planner"  # Nombre de la base de datos
+
     try:
         conn = pymysql.connect(
             host=db_host,
